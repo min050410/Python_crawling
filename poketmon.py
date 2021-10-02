f46612e089dfd48e731c9f93d67a240dbf7173a9
min050410 <min050410@gmail.com>
2021-10-02 23:53:03 +0900

    htmlparser

--- poketmon.py
+++ poketmon.py
@@ -1,13 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 # import json
 
 json_url = "https://www.pokemonkorea.co.kr/pokemon-unite/menu126?stype1=&stype2=&number=2315&mode=view"
 
 json_string = requests.get(json_url).text
 
-infos = json_string.select('h3-deco')
+# string 파일을 beautifulSoup 를 이용하여서 html 파일로 변환
+soup = BeautifulSoup(json_string, 'html.parser')
 
+# tags = soup.find_all('p')
 
-    
+for tag in soup.find_all('p'):
+    print(tag.text)  # 찾은 p 태그 내에 모든 txt를 출력
 
 
